316da3216b0324aef61b85f1e52de0f2dcd34653
Johanna Simfors <jsimfors@gmail.com>
2020-12-13 21:01:10 +0100

    2-player mode added

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,37 +3,60 @@ using namespace std;
 
 char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
-int checkwin();
+int result();
 void board();
-void gamePlay();
 
 int main(){
+    // Start by printing board
     board();
-    gamePlay();
-}
 
-int checkwin(){ 
-    // TODO create function that finds winner.
-    return 0;
-}
-
-void gamePlay(){
+    int player = 2;
     int choice;
-    int player = 1; // TODO: add more players.
     char mark;
-    cout << "Player " << player << ", choose your box" << endl;
+
+    while(result()==0){
+    // change to next players turn
+    player==2?player=1:player=2;
+
+    cout << "Player " << player << ", make your move by choosing one of the available numbers." << endl;
     cin >> choice;
     cout << "You chose " << choice << endl;
     mark=(player == 1) ? 'X' : 'O';
 
-    if (square[1] == '1')
-        square[1] = mark;
-    else
-    {
-        cout<<"Invalid move ";
+    if (isdigit(square[choice])){
+        square[choice] = mark;
+    }else{
+        cout << "Invalid move ";
     }
-    board();
 
+    board();
+    }
+}
+
+int result(){ 
+    // 0 = Game is ongoing
+    // 1 = player 1 wins
+    // 2 = player 2 wins
+    // 3 = No winner
+    int winner = 0;
+    // winner=(square[9] == 'X') ? 1 : 0;
+    // cout << "Winner func is: " << winner << endl;
+
+    // Horizontal wins:
+    // index: 1, 2, 3 ; 4, 5, 6 : 7, 8, 9
+    for(int i=1; i<=9;  i = i + 3){
+        if(square[i] == square[i+1] && square[i+1] == square[i+2]){
+            if(square[i]=='X'){
+                winner = 1;
+            }else{
+                winner = 2;
+            }
+        }
+    }
+
+    cout << "Congrats Player " << winner << ", you win" << endl;
+    return winner;
+    
 }
 
 void board(){
