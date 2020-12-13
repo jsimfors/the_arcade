4fb96a21575fc1c1b94385558c08c3c12af4428b
Johanna Simfors <jsimfors@gmail.com>
2020-12-13 21:38:28 +0100

    Draw-outcome added

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,60 +3,70 @@ using namespace std;
 
 char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
-int result();
+char result();
 void board();
 
 int main(){
     // Start by printing board
     board();
 
-    int player = 2;
+    char player = 'O';
     int choice;
     char mark;
 
-    while(result()==0){
+    while(result()=='n'){
     // change to next players turn
-    player==2?player=1:player=2;
+    player=='O'?player='X':player='O';
 
     cout << "Player " << player << ", make your move by choosing one of the available numbers." << endl;
     cin >> choice;
-    cout << "You chose " << choice << endl;
-    mark=(player == 1) ? 'X' : 'O';
+    //mark=(player == 1) ? 'X' : 'O';
 
     if (isdigit(square[choice])){
-        square[choice] = mark;
+        square[choice] = player;
     }else{
         cout << "Invalid move ";
     }
 
     board();
     }
+    result()!='d'?cout << "Congrats Player " << result() << ", you win" << endl: cout << "It's a draw!" << endl;
+
 }
 
-int result(){ 
-    // 0 = Game is ongoing
-    // 1 = player 1 wins
-    // 2 = player 2 wins
-    // 3 = No winner
-    int winner = 0;
-    // winner=(square[9] == 'X') ? 1 : 0;
-    // cout << "Winner func is: " << winner << endl;
+char result(){ 
+    // n = Null - game is ongoing
+    // X = player x wins
+    // O = player O wins
+    // d = draw, no winner
+    char winner = 'n';
 
-    // Horizontal wins:
-    // index: 1, 2, 3 ; 4, 5, 6 : 7, 8, 9
+    // Horizontal-wins i = 4, 7, 9
     for(int i=1; i<=9;  i = i + 3){
         if(square[i] == square[i+1] && square[i+1] == square[i+2]){
-            if(square[i]=='X'){
-                winner = 1;
-            }else{
-                winner = 2;
-            }
+            if(square[i]=='X') return 'x'; else return 'O';
         }
     }
 
-    cout << "Congrats Player " << winner << ", you win" << endl;
-    return winner;
-    
+    // Vertical-wins i = 1, 2, 3
+    for(int i=1; i<=3; i++){
+        if(square[i] == square[i+3] && square[i+3] == square[i+6]){
+            if(square[i]=='X') return 'x'; else return 'O';
+        }
+    }
+
+    // Diagonal-wins i 1, 5, 9 or i = 3, 5, 7.
+    if((square[1] == square[5] && square[5] == square[9]) || (square[3] == square[5] && square[5] == square[7])){
+        if(square[5]=='X') return 'x'; else return 'O';
+    }
+
+    // Check for draw
+    for(int i=1; i<=9;  i++){
+        if(isdigit(square[i])){
+            return 'n';
+        }
+    }
+    return 'd';
 }
 
 void board(){
